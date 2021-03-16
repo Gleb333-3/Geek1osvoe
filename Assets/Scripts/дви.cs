f1c41b1f97b8f93308a27476548afa25059c06d3
Gleb333-3 <glebefremenko23@gmail.com>
2021-03-16 15:50:47 +0300

    commit

---
+++ Assets/Scripts/дви.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class дви : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 __direction;
+
+    [SerializeField]
+    private float __speed;
+
+    private void Update()
+    {
+        __direction.x = Input.GetAxis("Horizontal");
+        __direction.z = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        var mor = __direction * (__speed * Time.deltaTime);
+        transform.Translate(mor);
+    }
+}
---
+++ Assets/bom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bom : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _GdeBom;
+
+    [SerializeField]
+    private Transform _Jon;
+
+    [SerializeField]
+    private Transform _bom;
+
+    [SerializeField]
+    private GameObject _yd;
+
+    [SerializeField]
+    private GameObject _Bom12;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_Bom12, _GdeBom.position, Quaternion.identity);
+        }
+        var di = Vector3.Distance(_Jon.position, _bom.position);
+        if (di < 1.5)
+        {
+            Destroy(_yd);
+            Debug.Log(di);
+        }
+    }
+}
